a7b5bf6b18e90959bf657e83bd6c543dd984d141
eve1ne <evelee@umich.edu>
2025-09-26 15:55:21 -0400

    Updated style and embedded urls on project pages

--- sql/data.sql
+++ sql/data.sql
@@ -1,6 +1,12 @@
 PRAGMA foreign_keys = ON;
 
-INSERT INTO projects(title, description, image_url, doc_url, demo_url)
+INSERT INTO projects(title, description, image_url, doc_url, demo_url, created_at)
 VALUES 
-    ('FANUC Internship', 'Designed and implemented scalable courses', 'PlaceholderImage.jpg', 'placeholder', 'placeholder'),
-    ('MedChecker', 'Designed and implemented scalable courses', 'MedChecker_Icon.png', 'placeholder', 'placeholder');
+    ('FANUC Internship', 'Designed and implemented scalable courses', 'PlaceholderImage.jpg', 'placeholder', 'placeholder', 'May 12th 2025 - August 8th 2025'),
+    ('MedChecker', 
+    'MedChecker is a mobile app designed to help parents safely manage medication for young children under six. The app provides clear dosage guidelines using 
+    Clark’s Rule, reliable information on common medications, and a simple, intuitive interface. By addressing the lack of dosage instructions for children on most 
+    medication bottles, MedChecker reduces parental stress and lowers the risk of medication errors, especially for families with limited access to healthcare. 
+    Unlike many existing apps that require subscriptions or focus on other demographics, MedChecker prioritizes accessibility and ease of use for new parents. 
+    In the long term, the app aims to expand to other groups, such as the elderly, while maintaining up-to-date medical data and strong security practices.', 
+    'MedChecker_Icon.png', 'MedChecker_Documentation.pdf', 'https://www.youtube.com/embed/277YGRorZEk?si=LLlEbl4FEMqCjluF', 'January 8th 2025 - April 22nd 2025');
