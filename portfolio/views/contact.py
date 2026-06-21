b639f8ece934d7c9c9d115c21a07e6b75fd042a8
eve1ne <evelee@umich.edu>
2026-06-21 19:08:49 -0400

    updated form to use formspree

--- portfolio/views/contact.py
+++ portfolio/views/contact.py
@@ -20,16 +20,3 @@ def show_contact():
     context = {}
 
     return flask.render_template("contact.html", **context)
-
-@portfolio.app.route('/contact/', methods=['POST'])
-def submit_contact_form():
-    """Handle contact form submission."""
-    # Process form data
-    name = flask.request.form.get('name')
-    email = flask.request.form.get('email')
-    message = flask.request.form.get('message')
-
-    portfolio.app.logger.info(f"Received contact form submission: Name={name}, Email={email}, Message={message}")
-
-    # Redirect or render success page
-    return flask.redirect(flask.url_for('show_contact'))
